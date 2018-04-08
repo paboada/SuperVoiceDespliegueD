5ffb199ef9cf6df12fb9cc921360ab512597f310
PABLO-LAPTOP\pablo <pabloandresboada@gmail.com>
2018-04-07 21:01:28 -0500

    sqs_batchmp3

--- sqs_batchmp3.py
+++ sqs_batchmp3.py
@@ -5,18 +5,24 @@ sqs = boto3.resource('sqs')
 # Get the queue. This returns an SQS.Queue instance
 queue = sqs.get_queue_by_name(QueueName='sqs_concursos')
 url_queue=queue.url
-messages = queue.receive_messages(QueueUrl=url_queue, AttributeNames=['All'], MessageAttributeNames=['All'], MaxNumberOfMessages=10) # adjust MaxNumberOfMessages if needed
-#if 'Messages' in messages: # when the queue is exhausted, the response dict contains no 'Messages' key
-print("Longitud de mensajes" , len(messages))
-for message in messages: # 'Messages' is a list
-# process the messages
+
+cont = 0
+while True
+    messages = queue.receive_messages(QueueUrl=url_queue, AttributeNames=['All'], MessageAttributeNames=['All'], MaxNumberOfMessages=10) # adjust MaxNumberOfMessages if needed
+    #if 'Messages' in messages: # when the queue is exhausted, the response dict contains no 'Messages' key
+    print("Longitud de mensajes" , len(messages))
+    for message in messages: # 'Messages' is a list
+    # process the messages
     #print(message)
     #print(message.body)
     #print(message.queue_url)
     #print(message.attributes)
     #print(message.message_attributes)
-    print(message.message_attributes['archivo_original']['StringValue'])
-    print(message.message_attributes['id_audio']['StringValue'])
+        print(message.message_attributes['archivo_original']['StringValue'])
+        print(message.message_attributes['id_audio']['StringValue'])
+        cont = cont + 1
+        if cont == 10
+        break
     #print(message.receipt_handle)
     #message.delete()
 # next, we delete the message from the queue so no one else will process it again
